e2480154fb2bb962700e86d16ab7aded76d006b2
Philip Perez Castro <jperez.c92@gmail.com>
2022-06-07 18:02:03 -0500

    test: update MovementsCollection

--- __tests__/modules/movements/containers/MovementsCollection/MovementsCollection.test.tsx
+++ __tests__/modules/movements/containers/MovementsCollection/MovementsCollection.test.tsx
@@ -1,12 +1,9 @@
-import { render, waitFor } from "@testing-library/react";
-import renderer from "react-test-renderer";
+import { render, screen } from "@testing-library/react";
 import { MovementsCollection } from "src/modules/movements/containers/MovementsCollection";
 import { MovementEndpoint } from "src/modules/movements/dto/MovementEndpoint";
 import { MovementType } from "src/modules/movements/models/MovementType";
 import { MovementsRepository } from "src/modules/movements/service/MovementsRepository";
 import * as useNodeMovementsRepository from "src/modules/movements/service/useNodeMovements.repository";
-import { MovementListStore } from "src/modules/movements/store/MovementListStore";
-import * as useMovementListState from "src/modules/movements/store/useMovementListState";
 import { OrderType } from "src/modules/shared/models/OrderType";
 
 const movementList: MovementEndpoint[] = Array.from({ length: 10 }).map(
@@ -24,59 +21,30 @@ const movementsRepository: MovementsRepository = {
   create: jest.fn(),
 };
 
-const movementListStore: MovementListStore = {
-  updateMovementList: jest.fn(),
-};
-
 jest
   .spyOn(useNodeMovementsRepository, "useNodeMovementsRepository")
   .mockReturnValue(() => movementsRepository);
-
-jest.spyOn(useMovementListState, "useMovementListState").mockReturnValue({
-  movementList,
-  movementListStore: () => movementListStore,
-});
 
 describe("MovementsCollection container", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  test("should match the snapshot", async () => {
-    const component = renderer
-      .create(
-        <MovementsCollection
-          movementType={MovementType.INCOME}
-          movementsRepository={useNodeMovementsRepository.useNodeMovementsRepository()}
-        />
-      )
-      .toJSON();
-
-    expect(component).toMatchSnapshot();
-  });
-
-  test("should first", async () => {
-    const component = render(
+  test("should list the movements", async () => {
+    render(
       <MovementsCollection
         movementType={MovementType.INCOME}
         movementsRepository={useNodeMovementsRepository.useNodeMovementsRepository()}
       />
     );
 
-    await waitFor(async () =>
-      expect(await component.findAllByRole("article")).toHaveLength(
-        movementList.length
-      )
-    );
+    expect(await screen.findByText("concept 1")).toBeInTheDocument();
+    expect(await screen.findByText("concept 10")).toBeInTheDocument();
 
     expect(movementsRepository.query).toHaveBeenCalledTimes(1);
     expect(movementsRepository.query).toHaveBeenCalledWith({
       movementType: MovementType.INCOME,
       order: OrderType.DESC,
     });
-    expect(movementListStore.updateMovementList).toHaveBeenCalledTimes(1);
-    expect(movementListStore.updateMovementList).toHaveBeenCalledWith(
-      movementList
-    );
   });
 });
