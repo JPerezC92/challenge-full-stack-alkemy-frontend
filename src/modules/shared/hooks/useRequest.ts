4c067798f84da34a78194b1f7b7543d163037205
Philip Perez Castro <jperez.c92@gmail.com>
2022-06-30 23:15:54 -0500

    refactor: useRequest hook

--- src/modules/shared/hooks/useRequest.ts
+++ src/modules/shared/hooks/useRequest.ts
@@ -1,12 +1,9 @@
 import React from "react";
-import { FAILED_TO_FETCH } from "src/modules/shared/utils/constants";
-import { useLoading } from "./useLoading";
 
 export const useRequest = (
   fn: (props: { abortController: AbortController }) => Promise<void>,
   deps: React.DependencyList
 ) => {
-  const { isLoading, finishLoading, startLoading } = useLoading(true);
   const abortControllerRef = React.useRef<AbortController>();
 
   React.useEffect(() => {
@@ -16,27 +13,13 @@ export const useRequest = (
 
     (async () => {
       try {
-        startLoading();
-
         await fn({ abortController });
-
-        finishLoading();
       } catch (error) {
-        if (error instanceof TypeError && error.message === FAILED_TO_FETCH) {
-          return console.log(error.message);
-        }
-
-        if ((error as DOMException).code !== DOMException.ABORT_ERR) {
-          throw error;
-        }
-      } finally {
-        finishLoading();
+        console.log(error);
       }
     })();
 
     return () => abortController.abort();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [finishLoading, startLoading, ...deps]);
-
-  return { isLoading };
+  }, [...deps]);
 };
